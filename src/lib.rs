373dad67edf027cf999039d6dd0e4b0c481febd6
Rajat Jindal <rajatjindal83@gmail.com>
2025-07-04 08:14:27 +0530

    change output msg
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,6 @@ fn handle_rusthttp(req: Request) -> anyhow::Result<impl IntoResponse> {
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
-        .body("Hello World!")
+        .body("Hello World from GitHub actions!")
         .build())
 }
